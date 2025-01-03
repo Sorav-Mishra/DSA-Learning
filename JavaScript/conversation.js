5f378dd971c5b600c758a67a46615c786a4338f9
Sorav-Mishra <abhiishekmishraa2025@gmail.com>
2025-01-03 06:46:30 +0530

    qqq

---
+++ JavaScript/conversation.js
@@ -0,0 +1,25 @@
+let score = "33";
+
+//console.log(typeof score);
+//console.log(typeof score);
+
+let newValue = Number(score);
+
+//console.log(typeof newValue);
+//console.log(newValue);
+
+//  "33" => 33
+//  "33abc" => NaN
+//  true => 1  false => 0
+
+let isLonggedIn = "abhi";
+console.log(typeof isLonggedIn);
+console.log(isLonggedIn);
+let boolean = Boolean(isLonggedIn);
+console.log(typeof boolean);
+console.log(boolean);
+
+// 1 => true
+// 0 => false
+// "" => ture
+// "abhi" => false
---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,29 @@
+// Premative Data type
+
+// String, Number, Boolean, Null, Undefined, BigInt, Symbol
+
+let userName = "solo";
+const age = 23;
+const isLonggedIn = true;
+let outTemp = null;
+let name;
+let bigNumber = 1234567890098976987987n;
+let id = Symbol(123);
+let anotherId = Symbol(123);
+console.log(id === anotherId);
+console.log(id);
+console.log(anotherId);
+
+// Array, Object, Function
+
+let superHeros = ["saktiman", "solo", "sfsjfjhkjj", "abhishrkhkjjk"];
+
+let user = {
+  name: "Solo",
+  age: "24",
+};
+
+function myUser() {
+  console.log("hello from solo");
+}
+//myUser();

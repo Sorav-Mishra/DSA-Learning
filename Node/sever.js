325ed7b462a2cad758b6566b7417f8a0a18bff8f
Sorav-Mishra <abhiishekmishraa2025@gmail.com>
2025-01-06 19:28:44 +0530

    sever

--- Node/sever.js
+++ Node/sever.js
@@ -1,15 +1,25 @@
 const http = require("http");
 
-const hostname = "Solo";
+const hostname = "127.0.0.1";
 
 const port = 3000;
 
 const sever = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-type", "textplain");
-  res.end("Hello ice tea");
+  if (req.url === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "Text/plain");
+    res.end("hello from solo");
+  } else if (req.url === "/login") {
+    res.statusCode === 200;
+    res.setHeader("Content-Type", "Text/plain");
+    res.end("404, not found");
+  } else if (req.url === "/order") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "Text/plain");
+    res.end("thanks for ordring chai");
+  }
 });
 
 sever.listen(port, hostname, () => {
-  console.log(`Sever is listening at http ${hostname} : ${port}`);
+  console.log(`Sever is listening at http://${hostname}:${port}`);
 });
